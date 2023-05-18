7461ec6098054130a94672239d86d14726abc349
memoros77 <memoros77@gmail.com>
2023-05-17 18:05:11 -0600

    Muestra de CI_CD Actividad 4_2 MIT

--- hello.test.js
+++ hello.test.js
@@ -1,6 +1,6 @@
 const hello = require("./hello");
 describe("My hello", () => {
   test("works", () => {
-    expect(hello.hello()).toEqual("Hola Mundo desde México MIT pro Guillermo Rosales");
+    expect(hello.hello()).toEqual("CI/CD en su maxima expresion, actualizammos local y se refleja en Cloud AWS");
   });
 });
