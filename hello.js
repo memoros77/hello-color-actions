829e48be68dbc5f9d69506dbb40a9b46ba2b350e
memoros77 <memoros77@gmail.com>
2023-05-17 18:15:28 -0600

    Correccion Archivos Test nuevo mensaje

--- hello.js
+++ hello.js
@@ -1,3 +1,3 @@
-const hello = () => "CI/CD en su maxima expresion, actualizammos local y se refleja en Cloud AWS"";
+const hello = () => "CI/CD en su maxima expresion, actualizammos local y se refleja en Cloud AWS";
 console.log(hello());
 exports.hello = hello;
